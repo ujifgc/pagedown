d99487a5091a0da312e368929e905d3ae17fa9bb
balpha <none@none>
2013-01-23 11:36:23 +0100

    prevent Alt+Gr from being captured by the undo/redo handler

--- Markdown.Editor.js
+++ Markdown.Editor.js
@@ -568,7 +568,7 @@
 
             var handled = false;
 
-            if (event.ctrlKey || event.metaKey) {
+            if ((event.ctrlKey || event.metaKey) && !event.altKey) {
 
                 // IE and Opera do not support charCode.
                 var keyCode = event.charCode || event.keyCode;
@@ -644,7 +644,7 @@
             util.addEvent(panels.input, "keypress", function (event) {
                 // keyCode 89: y
                 // keyCode 90: z
-                if ((event.ctrlKey || event.metaKey) && (event.keyCode == 89 || event.keyCode == 90)) {
+                if ((event.ctrlKey || event.metaKey) && !evt.altKey && (event.keyCode == 89 || event.keyCode == 90)) {
                     event.preventDefault();
                 }
             });
