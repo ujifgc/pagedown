18ba46a85d574213d16aaace85e40f8d693911fd
balpha <none@none>
2014-04-11 10:54:57 +0200

    add underscore to the problemUrlChars; encode problem chars in auto-links as well

--- Markdown.Converter.js
+++ Markdown.Converter.js
@@ -1277,7 +1277,7 @@ else
 
             //  autolink anything like <http://example.com>
             
-            var replacer = function (wholematch, m1) { return "<a href=\"" + m1 + "\">" + pluginHooks.plainLinkText(m1) + "</a>"; }
+            var replacer = function (wholematch, m1) { return "<a href=\"" + encodeProblemUrlChars(m1) + "\">" + pluginHooks.plainLinkText(m1) + "</a>"; }
             text = text.replace(/<((https?|ftp):[^'">\s]+)>/gi, replacer);
 
             // Email addresses: <address@domain.foo>
@@ -1356,7 +1356,7 @@ else
         //  attacklab: Utility functions
         //
 
-        var _problemUrlChars = /(?:["'*()[\]:]|~D)/g;
+        var _problemUrlChars = /(?:["'*()[\]:_]|~D)/g;
 
         // hex-encodes some unusual "problem" chars in URLs to avoid URL detection problems 
         function encodeProblemUrlChars(url) {
