2b260eae9022360291c3927b3db0fc3e0d7e13e3
balpha <none@none>
2014-04-11 10:28:50 +0200

    don't capture the character after the bold/italic end (http://meta.stackoverflow.com/q/126382/)

--- Markdown.Converter.js
+++ Markdown.Converter.js
@@ -1079,11 +1079,11 @@ else
         function _DoItalicsAndBold(text) {
 
             // <strong> must go first:
-            text = text.replace(/([\W_]|^)(\*\*|__)(?=\S)([^\r]*?\S[\*_]*)\2([\W_]|$)/g,
-            "$1<strong>$3</strong>$4");
+            text = text.replace(/([\W_]|^)(\*\*|__)(?=\S)([^\r]*?\S[\*_]*)\2(?=[\W_]|$)/g,
+            "$1<strong>$3</strong>");
 
-            text = text.replace(/([\W_]|^)(\*|_)(?=\S)([^\r\*_]*?\S)\2([\W_]|$)/g,
-            "$1<em>$3</em>$4");
+            text = text.replace(/([\W_]|^)(\*|_)(?=\S)([^\r\*_]*?\S)\2(?=[\W_]|$)/g,
+            "$1<em>$3</em>");
 
             return text;
         }
